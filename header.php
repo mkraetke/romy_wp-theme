cf89dc6e7dee4b535690b83144bc001b5ec244ba
Martin Kraetke <mkraetke@gmx.de>
2019-06-22 17:21:52 +0200

    fix wellformedness error

--- header.php
+++ header.php
@@ -16,25 +16,14 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<link rel="profile" href="https://gmpg.org/xfn/11"/>
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/lib/bootstrap/css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/lib/bootstrap/css/bootstrap.min.css"/>
 	<?php wp_head(); ?>
 </head>
-
 <body <?php body_class(); ?>>
-  <div class="bg <?php if (! is_home()){ echo 'bgnone';} ?>">
-  <?php
+	<div class="bg <?php if (! is_home()){ echo 'bgnone';} ?>">
+		<?php
 			include('navbar.php');
-			?>
-  
-  </div>
-
-
-<div id="page" class="site">
-
-
-	<header id="masthead" class="site-header">
-		
-			
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content <?php if ( is_home() ){ echo 'site-home'; } ?>">
+		?>  
+	</div>
+	<div id="page" class="site">
+		<div id="content" class="site-content <?php if ( is_home() ){ echo 'site-home'; } ?>">
